eae46644c9ce1dc54c6999d6385f6ede3225fbc9
Flameagle <seiftamer606@gmail.com>
2025-12-19 15:58:48 +0200

    program is finished for now

--- calculator.cpp
+++ calculator.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+int add(int x,int y){return x+y;}
+
 int main(){
-    int x,y,o;
+    int x,y;string o,a;
+    while(true){
+        cout<<"Enter the equation: ";cin>>x>>o>>y;
+        if(o=="+"){cout<<x+y;}
+        cout<<"\nTry again?(y/n): ";cin>>a;
+        if(a=="n"){break;}
+        
+    }
 
     return 0;
 }
