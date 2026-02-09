d2c1a2ec0f2c068bb9d5386ca19d0fc6ef7fe5c5
Flameagle <seiftamer606@gmail.com>
2026-02-09 18:11:17 +0200

    used switch instead of else-if

--- calculator.cpp
+++ calculator.cpp
@@ -8,19 +8,23 @@ int mult(int x,int y){return x*y;}
 int divi(int x,int y){return x/y;}
 
 int main(){
-    int x,y;string o;
+    int x,y;char o;
     while(true){
-        string a="b";
+        char a='b';
         cout<<"Enter the equation: ";cin>>x>>o>>y;
-        //if(cin.fail()){cout<<"Invalid data type!\n";cin.clear();continue;}
-        if(o=="+"){cout<<add(x,y);}
-        else if(o=="-"){cout<<sub(x,y);}
-        else if(o=="*"){cout<<mult(x,y);}
-        else if(o=="/"){cout<<divi(x,y);}
-        while(a!="y" and a!="n"){cout<<"\nTry again?(y/n): ";cin>>a;}
-        //if(a!="y" or a!="n"){cout<<"This input is invalid!\nEnter y or n.\n";}
-        if(a=="n"){break;}
-        a="b";
+        switch(o){
+            case '+':cout<<add(x,y);
+            break;
+            case '-':cout<<sub(x,y);
+            break;
+            case '*':cout<<mult(x,y);
+            break;
+            case '/':cout<<divi(x,y);
+            break;
+        }
+        while(a!='y' and a!='n'){cout<<"\nTry again?(y/n): ";cin>>a;}
+        if(a=='n'){break;}
+        a='b';
         
     }
 
